104c3badaea7555138cb6d8a31cf2dc9b32ab2d7
Ed Liveikis <ed@ardentblue.com>
2020-07-29 21:25:00 +0200

    Feature/arcus connection design (Many jiras)

--- one/arcus/arcus.h
+++ one/arcus/arcus.h
@@ -2,30 +2,35 @@
 
 #include <functional>
 
+
 namespace one {
 
-class Message;
 class Array;
-///
-/// Arcus is the main interface for an Arcus API Connection.
-/// It manages a TCP listen connection, allowing a single Agent client to connect
-/// and send/receive messages.
-///
-class Arcus
+class Connection;
+class Message;
+class Socket;
+
+// An Arcus Server is designed for use by a Game. It allows an Arcus One Agent
+// to connect and communicate with the game.
+class Server final
 {
 public:
-    Arcus(/* args */);
-    ~Arcus();
+    Server(/* args */);
+    ~Server();
+
+    int listen(unsigned int port);
 
     // Process pending received and outgoing messages. Any incoming messages are
     // validated according to the Arcus API version standard, and callbacks, if
     // set, are called. Messages without callbacks set are dropped and ignored.
-    void update(int* error);
+    int update();
 
     int status() const;
 
     //------------------------------------------------------------------------------
     // Callbacks to be notified of all possible incoming Arcus messages.
+
+    // Todo: update functions to match complete list from One API v2.
 
     void set_soft_stop_callback(std::function<void(int)>);
 
@@ -45,7 +50,7 @@ public:
     //------------------------------------------------------------------------------
     // Outgoing.
 
-    // WIP.
+    // Todo: update functions to match complete list from One API v2.
 
     // notify one of state change
     // send metadata (key value pairs)
@@ -58,6 +63,41 @@ public:
     //     mode,
     //     version)
     void send_server_info(Message&);
+
+private:
+    Socket *_listen_socket;
+    Socket *_client_socket;
+    Connection *_client_connection;
+};
+
+// The Arcus Client is used by an Arcus One Agent to connect to an Arcus Server.
+class Client final
+{
+public:
+    Client(/* args */);
+    ~Client();
+
+    int connect(const char * ip, unsigned int port);
+
+    int update();
+
+    //-------------------
+    // Outgoing Messages.
+
+    // todo.
+
+    // send_soft_stop(...)
+
+    // ...
+
+    //-------------------
+    // Incoming Messages.
+
+    // todo.
+
+private:
+    Socket *_socket;
+    Connection *_connection;
 };
 
 } // namespace one
--- one/arcus/internal/connection.cpp
+++ one/arcus/internal/connection.cpp
@@ -1,5 +1,18 @@
+#include "connection.h"
+
+#include "socket.h"
+
 namespace one {
 
+Connection::Connection(Socket& socket, size_t max_messages_in, size_t max_messages_out):
+    _socket(socket)
+{
 
+}
+
+Connection::~Connection()
+{
+
+}
 
 } // namespace one
