bc495a34e7e3a80f7a9c8a3821905a5fc7a01ba3
Martin Turcotte <tirpidz@gmail.com>
2020-08-04 21:59:16 +0200

    Feature/opcode handling (OCS-334)

--- one/agent/main.cpp
+++ one/agent/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <version.h>
+#include <one/arcus/internal/version.h>
 
 int main() {
     std::cout << "Agent -> project name: " << ONE_NAME << " version: " << ONE_VERSION << std::endl;
