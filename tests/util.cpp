32dd6dc487f2ead7fea14ac10f8414e84684e701
Ed Liveikis <ed@ardentblue.com>
2020-08-10 16:52:07 +0200

    Feature/agent connection testing (OCS-260)

--- tests/util.cpp
+++ tests/util.cpp
@@ -9,4 +9,13 @@ void sleep(int ms) {
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
+void for_sleep(int count, int ms_per_loop, std::function<bool()> cb) {
+    for (int i = 0; i < count; i++) {
+        if (cb()) {
+            break;
+        }
+        sleep(ms_per_loop);
+    }
+}
+
 }  // namespace one
--- tests/util.h
+++ tests/util.h
@@ -1,7 +1,14 @@
 #pragma once
 
+#include <functional>
+
 namespace one {
 
 void sleep(int ms);
+
+// Loops for given number of times, calling the given function. If the function
+// returns true, then the loop breaks. If the function returns false, then
+// the loop sleeps ms_per_loop milliseconds and then continues.
+void for_sleep(int count, int ms_per_loop, std::function<bool()> cb);
 
 }  // namespace one
