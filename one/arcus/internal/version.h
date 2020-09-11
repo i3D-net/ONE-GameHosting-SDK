56b34c7980d28d1ae0d5f45af16ecc4c15e3f762
Edward Liveikis <ed@ardentblue.com>
2020-09-11 12:59:40 +0200

    Remove unnecessary clang format toggle.

--- one/arcus/internal/version.h
+++ one/arcus/internal/version.h
@@ -2,9 +2,11 @@
 
 #define ONE_NAME "OneGameHostingSDK"
 #define ONE_VERSION "1.0.0"
+// clang-format off
 #define ONE_VERSION_MAJOR 1
 #define ONE_VERSION_MINOR 0
 #define ONE_VERSION_PATCH 0
+// clang-format on
 
 namespace i3d {
 namespace one {
