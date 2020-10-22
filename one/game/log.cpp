7ac680ee5473a5ee49b9ee9963994d0baa556b11
Martin Turcotte <tirpidz@gmail.com>
2020-10-23 00:37:04 +0200

    add log to file (OCS-256)

--- one/game/log.cpp
+++ one/game/log.cpp
@@ -1,15 +1,68 @@
 #include <one/game/log.h>
 
 #include <iostream>
+#include <fstream>
 
 namespace one_integration {
 
-void log_info(const std::string &message) {
+void LogCentral::log_info(const std::string &message) {
+    if (!log_file_info(_log_file, message)) {
+        log_console_info(message);
+    }
+}
+
+void LogCentral::log_error(const std::string &message) {
+    if (!log_file_error(_log_file, message)) {
+        log_console_error(message);
+    }
+}
+
+void LogCentral::log_console_error(const std::string &message) {
     std::cout << message << std::endl;
 }
 
-void log_error(const std::string &message) {
+void LogCentral::log_console_info(const std::string &message) {
     std::cerr << message << std::endl;
 }
+
+bool LogCentral::log_file_info(const std::string &file_name, const std::string message) {
+    if (_log_file.empty()) {
+        return false;
+    }
+
+    std::ofstream file;
+    file.open(file_name, std::ios_base::app);
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    file << "INFO : " << message << std::endl;
+    file.close();
+    return true;
+}
+
+bool LogCentral::log_file_error(const std::string &file_name, const std::string message) {
+    if (_log_file.empty()) {
+        return false;
+    }
+
+    std::ofstream file;
+    file.open(file_name, std::ios_base::app);
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    file << "ERROR: " << message << std::endl;
+    file.close();
+    return true;
+}
+
+void LogCentral::set_log_file(const std::string &log_file) {
+    _log_file = log_file;
+}
+
+std::string LogCentral::_log_file = "";
 
 }  // namespace one_integration
