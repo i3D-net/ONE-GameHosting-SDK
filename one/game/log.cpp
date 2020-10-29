5756833bba0709e2d499e5e670036daca99be573
Martin Turcotte <tirpidz@gmail.com>
2020-10-29 10:20:46 -0400

    add timestamp in game logs

--- one/game/log.cpp
+++ one/game/log.cpp
@@ -1,5 +1,6 @@
 #include <one/game/log.h>
 
+#include <ctime>
 #include <iostream>
 #include <fstream>
 
@@ -37,7 +38,7 @@ bool LogCentral::log_file_info(const std::string &filename, const std::string me
         return false;
     }
 
-    file << "INFO : " << message << std::endl;
+    file << timestamp() << " INFO : " << message << std::endl;
     file.close();
     return true;
 }
@@ -54,13 +55,21 @@ bool LogCentral::log_file_error(const std::string &filename, const std::string m
         return false;
     }
 
-    file << "ERROR: " << message << std::endl;
+    file << timestamp() << " ERROR: " << message << std::endl;
     file.close();
     return true;
 }
 
 void LogCentral::set_log_filename(const std::string &log_file) {
     _log_filename = log_file;
+}
+
+std::string LogCentral::timestamp() {
+    time_t now;
+    time(&now);
+    char buf[sizeof "2020-10-29T10:17:00Z"];
+    strftime(buf, sizeof buf, "%FT%TZ", gmtime(&now));
+    return std::string(buf);
 }
 
 std::string LogCentral::_log_filename = "";
