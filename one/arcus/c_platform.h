cb491d4cabdc6da307ce1ee602ee3d84bca20ddd
Martin Turcotte <tirpidz@gmail.com>
2021-02-16 17:11:06 -0500

    fix platform defines
    * need to check if defined && true

--- one/arcus/c_platform.h
+++ one/arcus/c_platform.h
@@ -1,14 +1,22 @@
 #pragma once
 
-#if defined(PLATFORM_WINDOWS)
+// Symbol export management.
+#if defined(PLATFORM_LINUX) && PLATFORM_LINUX
+    #ifndef ONE_LINUX
+        #define ONE_LINUX
+    #endif
+
+    #ifndef ONE_EXPORT
+        #define ONE_EXPORT __attribute__((visibility("default")))
+    #endif
+
+    #include <stdlib.h>
+#elif defined(PLATFORM_WINDOWS) && PLATFORM_WINDOWS
     #define ONE_UNREAL_WINDOWS
     // This disable rapidjson warnings on Windows where __gnuc__
     // is not defined.
     #pragma warning(disable : 4668)
-#endif
 
-// Symbol export management.
-#if defined(PLATFORM_WINDOWS) || defined(WINDOWS)
     #ifndef ONE_WINDOWS
         #define ONE_WINDOWS
     #endif
@@ -16,7 +24,15 @@
     #ifndef ONE_EXPORT
         #define ONE_EXPORT __declspec(dllexport)
     #endif
-#else // If not windows, Linux.
+#elif defined(WINDOWS)
+    #ifndef ONE_WINDOWS
+        #define ONE_WINDOWS
+    #endif
+
+    #ifndef ONE_EXPORT
+        #define ONE_EXPORT __declspec(dllexport)
+    #endif
+#else  // If not windows, Linux.
     #ifndef ONE_LINUX
         #define ONE_LINUX
     #endif
