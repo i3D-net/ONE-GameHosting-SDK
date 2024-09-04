b64c84c49b565bd988452603614375a5e4bd203f
Rob Bothof <rbothof@xs4all.nl>
2024-09-04 11:01:51 +0200

    discontinue 32bit support in buildscripts

--- tools/build_debug_linux_a_32.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cmake -S ../ -B ../build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32
-cmake --build ../build/ --target all
--- tools/build_debug_linux_so_32.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cmake -S ../ -B ../build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32 -DSHARED_ARCUS_LIB=ON
-cmake --build ../build/ --target all
